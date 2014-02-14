3d464036ad20e11bceab2c618c3dea5d15862959
slc85 <stevenjcundiff@gmail.com>
2014-02-14 13:00:37 -0700

    basic blog no postgre

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickBlog::Application.config.secret_token = 'eff3a6db27d63fd9e452bc59da51b4d4471178ba7ae794beb3b094e5014303926a24303e0b2e523ac4029dd2a9c0adb147aac7b3ca90f30ba4cc0469f25a8640'
